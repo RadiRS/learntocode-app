eae988bc323859e4823ecafbfe60a507baca1bf0
Radi <radi.rusadi.s@gmail.com>
2019-03-26 22:20:31 +0700

    Instaling styled components

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import styled from 'styled-components';
 
 class WelcomeScreen extends Component {
   static navigationOptions = () => ({
-      header: null
-    })
+    header: null
+  });
 
   render() {
     return (
